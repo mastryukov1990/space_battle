f91652ed50498f56cab1662a0c38149acdaaf398
mastryukov1990 <55547473+mastryukov1990@users.noreply.github.com>
2019-12-05 23:26:48 +0300

    Add files via upload

--- land.py
+++ land.py
@@ -1,131 +1,137 @@
-# Pygame шаблон - скелет для нового проекта Pygame
 import pygame
 import random
 from tkinter import*
 from button import*
 from MENUR import*
-Mob_size = 2
+Mob_size = 10
 super_sec = 10
 super_energy = 3
-number_of_mobs = 100 #20000 - MAX
+number_of_mobs = 10  # 20000-MAX
 WIDTH = 1366
-on = 1 #Mobi
+on = 1  # Mobi
 mob_lives = 10
 touch = 0
-shield = 1
+shield = 0
 Game_mode = 1
-WHITE = (255,255,255)
-n = 1 #lives_Players
+WHITE = (255, 255, 255)
+n = 1  # lives_Players
 HEIGHT = 720
 FPS = 50
 Size_B = 6
-Size_A = 100
+Size_A = 6
 rnd = random.randrange
-Nuber_of_STRIKE = 3
+Nuber_of_STRIKE = 5
 # Задаю цвета
 f = open('Game_mode.txt')
 Game_mode = int(f.read())
 print(Game_mode)
-print( on)
+print(on)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+
 def add_Mobi():
-    m = Mobi(p1,p2)
+    m = Mobi(p1, p2)
     all_sprites.add(m)
     mobi.add(m)
+
+
 class S_bullet(pygame.sprite.Sprite):
-    def __init__(self, x, y, stripe)
+    def __init__(self, x, y, stripe):
         pygame.sprite.Sprite.__init__(self)
         self.stripe = stripe
-        self.image = pygame.Surface((s_sizeB,s_sizeA))
+        self.image = pygame.Surface((s_sizeB, s_sizeA))
         self.image.fill(WHITE)
         self.rect = self.image.get_rect()
-        self.rect.y = y - 35
+        self.rect.y = y - s_sizeB/2
         self.rect.x = x
         self.Vy = 10
         self.Vx = 10
         self.s_live = s_live
+
     def update(self):
         if self.stripe == 1:
-            self.rect.x = self.rect.x -self.Vx
+            self.rect.x = self.rect.x - self.Vx
             if self.rect.left < 0:
                 self.kill()
                 p1.s_energy = p1.s_energy + 1
         if self.stripe == 2:
-            self.rect.x = self.rect.x +self.Vx
-            if self.rect.right> WIDTH:
+            self.rect.x = self.rect.x + self.Vx
+            if self.rect.right > WIDTH:
                 self.kill()
-                p2.s_energy = p2.s_energy +1
+                p2.s_energy = p2.s_energy + 1
+
+
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, x,y,stripe):
+    def __init__(self, x, y, stripe):
         pygame.sprite.Sprite.__init__(self)
         self.stripe = stripe
-        self.image = pygame.Surface((Size_B,Size_A))
+        self.image = pygame.Surface((Size_B, Size_A))
         self.image.fill(BLUE)
         self.rect = self.image.get_rect()
-        self.rect.y = y - 35
+        self.rect.y = y - Size_B/2
         self.rect.x = x
         self.Vx = 10
         self.Vy = 10
+
     def update(self):
         if self.stripe == 1:
-            self.rect.x = self.rect.x -self.Vx
+            self.rect.x = self.rect.x - self.Vx
             if self.rect.left < 0:
                 self.kill()
                 p1.energy = p1.energy + 1
         if self.stripe == 2:
-            self.rect.x = self.rect.x +self.Vx
-            if self.rect.right> WIDTH:
+            self.rect.x = self.rect.x + self.Vx
+            if self.rect.right > WIDTH:
                 self.kill()
-                p2.energy = p2.energy +1
-            
-        
-        
-        
+                p2.energy = p2.energy + 1
+
+
 class Mobi(pygame.sprite.Sprite):
-    def __init__(self, p1,p2):
+    def __init__(self, p1, p2):
         pygame.sprite.Sprite.__init__(self)
         self.p1 = p1
         self.p2 = p2
-        self.image = pygame.Surface((Mob_size,Mob_size))
+        self.image = pygame.Surface((Mob_size, Mob_size))
         self.image.fill(RED)
         self.rect = self.image.get_rect()
-        self.rect.y = rnd(0, 720)
+        self.rect.y = rnd(-80, 0)
         self.rect.x = rnd(10, 1360)
-        self.speedy = rnd(1,2)
-        self.speedx = rnd(-1,1)
+        self.speedy = rnd(1, 2)
+        self.speedx = rnd(-1, 1)
         self.live = mob_lives
         self.touch = touch
+
     def update(self):
         self.rect.y = self.rect.y + self.speedy
         self.rect.x = self.rect.x + self.speedx
-        if self.rect.y >700 or self.rect.x > WIDTH or self.rect.x< 0 :
-            self.rect.y = rnd(-80,0)
-            self.rect.x = rnd(100,WIDTH)
+        if self.rect.y > 700 or self.rect.x > WIDTH or self.rect.x < 0:
+            self.rect.y = rnd(-80, 0)
+            self.rect.x = rnd(-10, WIDTH)
         if self.live == 0:
-            self.rect.y = rnd(-80,0)
-            self.rect.x = rnd(100,WIDTH)
+            self.rect.y = rnd(-80, 0)
+            self.rect.x = rnd(100, WIDTH)
         if touch:
 
             hit = pygame.sprite.collide_rect(self, self.p1)
-                
+
             if hit:
-                if self.rect.x > p1.rect.x:
+                if self.rect.left > p1.rect.right:
                     self.p1.rect.right = self.rect.left
-                else:
+                elif self.rect.right < p1.rect.left:
                     self.p1.rect.left = self.rect.right
             hit = pygame.sprite.collide_rect(self, self.p1)
             if hit:
-                
+
                 if self.rect.y < p1.rect.y:
                     self.p1.rect.top = self.rect.bottom
                 else:
                     self.p1.rect.bottom = self.rect.top
             hit = pygame.sprite.collide_rect(self, self.p2)
-                
+
             if hit:
                 if self.rect.x > p2.rect.x:
                     self.p2.rect.right = self.rect.left
@@ -133,11 +139,13 @@ class Mobi(pygame.sprite.Sprite):
                     self.p2.rect.left = self.rect.right
             hit = pygame.sprite.collide_rect(self, self.p2)
             if hit:
-                
+
                 if self.rect.y < p2.rect.y:
                     self.p2.rect.top = self.rect.bottom
                 else:
                     self.p2.rect.bottom = self.rect.top
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, stripe, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -152,117 +160,122 @@ class Player(pygame.sprite.Sprite):
         self.super_energy = super_energy
         self.super_sec = super_sec
         self.rect = self.image.get_rect()
-        self.rect.center = (x/ 2 , y / 2)
+        self.rect.center = (x / 2, y / 2)
         self.Vx = 5
         self.Vy = 5
         self.speedx = 0
         self.speedy = 0
         self.live = n
         self.energy = Nuber_of_STRIKE
+
     def update(self):
-        
         self.speedx = 0
         self.speedy = 0
+
         keystate = pygame.key.get_pressed()
         if self.stripe == 1:
             if self.rect.right > WIDTH:
                 self.rect.right = WIDTH
             if self.rect.left < WIDTH/2:
                 self.rect.left = WIDTH/2
-            if  keystate[pygame.K_LEFT]:
+            if keystate[pygame.K_LEFT]:
                 self.speedx = -5
-            if  keystate[pygame.K_UP]:
-                self.speedy = -5 
-            if  keystate[pygame.K_DOWN]:
+            if keystate[pygame.K_UP]:
+                self.speedy = -5
+            if keystate[pygame.K_DOWN]:
                 self.speedy = 5
-            if  keystate[pygame.K_RIGHT]:
+            if keystate[pygame.K_RIGHT]:
                 self.speedx = 5
-            
         if self.stripe == 2:
             if self.rect.right > WIDTH/2:
                 self.rect.right = WIDTH/2
             if self.rect.left < 0:
                 self.rect.left = 0
-            if  keystate[pygame.K_a]:
+            if keystate[pygame.K_a]:
                 self.speedx = -5
-            if  keystate[pygame.K_w]:
-                self.speedy = -5 
-            if  keystate[pygame.K_s]:
+            if keystate[pygame.K_w]:
+                self.speedy = -5
+            if keystate[pygame.K_s]:
                 self.speedy = 5
-            if  keystate[pygame.K_d]:
+            if keystate[pygame.K_d]:
                 self.speedx = 5
         self.rect.x += self.speedx
-        self.rect.y += self.speedy 
+        self.rect.y += self.speedy
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
-        if self.rect.y> HEIGHT-50:
+        if self.rect.y > HEIGHT - 50:
             self.rect.y = HEIGHT - 50
         if self.rect.top < 0:
             self.rect.top = 0
-    
+
     def shoot(self):
         if self.energy > 0:
             if self.stripe == 1:
-                bullet = Bullet(self.rect.left - 3, self.rect.centery,self.stripe)
+                bullet = Bullet(self.rect.left - 8,
+                                self.rect.centery,
+                                self.stripe
+                                )
                 all_sprites.add(bullet)
                 bullets1.add(bullet)
             if self.stripe == 2:
-                bullet = Bullet(self.rect.right, self.rect.centery,self.stripe)
+                bullet = Bullet(self.rect.right + 8,
+                                self.rect.centery,
+                                self.stripe)
                 all_sprites.add(bullet)
                 bullets2.add(bullet)
             self.energy = self.energy - 1
+
     def super_shoot(self):
         if self.super_energy > 0:
             if self.stripe == 1:
-                s_bullet = S_bullet(self.rect.left - 3, self.rect.centery,self.stripe)
+                s_bullet = S_bullet(self.rect.left - 8,
+                                    self.rect.centery,
+                                    self.stripe
+                                    )
                 all_sprites.add(s_bullet)
                 bullets1.add(s_bullet)
             if self.stripe == 2:
-                s_bullet = S_bullet(self.rect.right, self.rect.centery,self.stripe)
+                s_bullet = S_bullet(self.rect.right,
+                                    self.rect.centery,
+                                    self.stripe
+                                    )
                 all_sprites.add(s_bullet)
                 bullets2.add(s_bullet)
             self.super_energy = self.super_energy - self.super_sec
-                
-
-            
-            
-        
-        
-
 bullets1 = pygame.sprite.Group()
 bullets2 = pygame.sprite.Group()
-mobi= pygame.sprite.Group()
+mobi = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 players = pygame.sprite.Group()
 if Game_mode == 0:
-    p1 = Player(1, WIDTH*3/2 ,200)
+    p1 = Player(1, WIDTH * 3/2, 200)
     all_sprites.add(p1)
     players.add(p1)
     all_sprites.add(p1)
-    p2 = Player(2, WIDTH/2 ,600)
+    p2 = Player(2, WIDTH/2, 600)
     all_sprites.add(p2)
     players.add(p2)
     all_sprites.add(p2)
 if Game_mode == 1:
-    p1 = Player(1, WIDTH*3/2 ,200)
+    p1 = Player(1, WIDTH*3/2, 200)
     all_sprites.add(p1)
     players.add(p1)
     all_sprites.add(p1)
-    p2 = Player(1, WIDTH*3/2 ,200)
+    p2 = Player(1, WIDTH*3/2, 200)
     all_sprites.add(p2)
     players.add(p2)
     all_sprites.add(p2)
 # Создаем игру и окно
+
+
 def play():
     pygame.init()
     pygame.mixer.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("My Game")
     clock = pygame.time.Clock()
-
-    
     if on == 1:
         for i in range(number_of_mobs):
             add_Mobi()
@@ -272,32 +285,32 @@ def play():
     while running:
         # Держим цикл на правильной скорости
         clock.tick(FPS)
-        hits = pygame.sprite.groupcollide(players, mobi, False,  1-shield)
+        hits = pygame.sprite.groupcollide(players, mobi, 1-shield,  1-shield)
         if hits and shield == 0:
             running = False
             add_Mobi()
         hitts1 = pygame.sprite.groupcollide(mobi, bullets1, True, True)
         if hitts1:
-            p1.energy = p1.energy +1
-            add_Mobi()
+            p1.energy = p1.energy + 1
+
         hitts2 = pygame.sprite.groupcollide(mobi, bullets2, True, True)
         if hitts2:
-            p2.energy = p2.energy +1
-            
-        hittts1= pygame.sprite.groupcollide(players , bullets1, True, False)
+            p2.energy = p2.energy + 1
+
+        hittts1 = pygame.sprite.groupcollide(players, bullets1, True, False)
         if hittts1:
             running = False
-        hittts2 = pygame.sprite.groupcollide(players , bullets2, True, False)
+        hittts2 = pygame.sprite.groupcollide(players, bullets2, True, False)
         if hittts2:
             running = False
         if on == 1:
-            for hit in hitts1: 
-                m = Mobi(p1,p2)
+            for hit in hitts1:
+                m = Mobi(p1, p2)
                 all_sprites.add(m)
                 mobi.add(m)
         if on == 1:
-            for hit in hitts2: 
-                m = Mobi(p1,p2)
+            for hit in hitts2:
+                m = Mobi(p1, p2)
                 all_sprites.add(m)
                 mobi.add(m)
         # Ввод процесса (события)
@@ -309,19 +322,12 @@ def play():
                 if event.key == pygame.K_RALT:
                     p1.shoot()
 
-                if event.key == pygame.K_LALT:
+                if event.key == pygame.K_q:
                     p2.shoot()
-                    
-            
-        
-        # Обновление
+
         all_sprites.update()
-        
-        # Рендеринг
         screen.fill(BLACK)
         all_sprites.draw(screen)
-        # После отрисовки всего, переворачиваем экран
         pygame.display.flip()
-
     pygame.quit()
 play()
--- menu_place.py
+++ menu_place.py
@@ -1,9 +1,12 @@
 from tkinter import *
+
 m = 'm'
 
-l_con = ['size','number:','fire:', 'on:','touch','Number of strikes']
+l_con = ['size','number:','fire:', 'on:','touch','Number of strikes:']
+spins = [1,2,5,6]
+checks = [3,4]
 class Settings:
-    def __init__(self,l_con):
+    def __init__(self,l_con,spins,checks):
         self.labels ={}
         self.k = 0
         self.x = 0.1
@@ -14,41 +17,98 @@ class Settings:
         self.y = 0
         self.l_con = l_con
         self.checkbutton = 3
-        
-       
+        self.spins = spins
+        self.spinboxes = {}
+        self.checks = checks
+        self.checkbuttons = {}
+        self.cvar = 1
+        for i in self.spins:
+            self.spinboxes.update({self.l_con[i-1]:{}})
+        for i in self.checks:
+            self.checkbuttons.update({self.l_con[i-1]:{}})
         for i in self.l_con:
             self.labels.update({i:{}})
         for i in self.labels:
             self.y = 0.1 + self.k/20
-            self.labels[i].update({self.label:{'x':self.x,'width':self.width,'hieght': self.hieght},self.spinbox:{}})
+            self.labels[i].update({
+                                self.label:{'x':self.x,
+                                'width':self.width,
+                                'hieght': self.hieght},
+                                self.spinbox:  {},
+                                self.checkbutton : {}
+                                })
             self.labels[i][self.label].update({'y':self.y})
             print(self.labels[i])
-            self.k = self.k+ 1
-        self.spinboxes ={self.l_con[0]:{},self.l_con[1]:{}}
+            self.k = self.k + 1
+        
         for t in self.spinboxes:
             self.labels[t].update({self.spinbox:{}})
-            self.labels[t][self.spinbox].update({'y': self.labels[t][self.label]['y'],'x':self.labels[t][self.label]['x']+ self.width} )
+            self.labels[t][self.spinbox].update({
+                                'y': self.labels[t][self.label]['y'],  
+                                'x':self.labels[t][self.label]['x']
+                                 + self.width
+                                })
             print(self.labels[t])
-        self.checkbuttons = {self.l_con[2]:{},self.l_con[3]:{}}
+        self.checkbuttons = {
+            self.l_con[2]:{},
+            self.l_con[3]:{}
+            }
         for t in self.checkbuttons:
             self.labels[t].update({self.checkbutton:{}})
-            self.labels[t][self.checkbutton].update({'y': self.labels[t][self.label]['y'],'x':self.labels[t][self.label]['x']+2* self.width} )
+            self.labels[t][self.checkbutton].update({
+                                            'y': self.labels[t][self.label]['y'],
+                                            'x': self.labels[t][self.label]['x'] 
+                                            +  self.width,
+                                            'cvar' : self.cvar} )
             print(self.labels[t])
-
+        
     
-    
-        b = Button(text ='Push',bg='red').place(relx=0.4, rely=0.8, relwidth = 0.2, relheight =0.05)
+        
+        b = Button(text ='Push',bg='red').place(
+                                relx=0.4, 
+                                rely=0.8, 
+                                relwidth = 0.2, 
+                                relheight =0.05
+                                )
         for i in self.l_con:
             l = self.labels[i][self.label]
-            Label(text = i, bg = 'white').place(relx =l['x'], rely = l['y'],relwidth =l['width'])
+            Label(text = i, bg = 'white').place(
+                                        relx =l['x'], 
+                                        rely = l['y'],
+                                        relwidth =l['width']
+                                        )
             if self.labels[i][self.spinbox]:
                 s = self.labels[i][self.spinbox]
-                Spinbox(bg = 'yellow',from_=1, to = 20000).place(relx = s['x'], rely = s['y'], relwidth = 0.2, relheight = 0.05)
-                
+                Spinbox(bg = 'yellow',from_=1, to = 20000).place(
+                            relx = s['x'], 
+                            rely = s['y'],
+                            relwidth = 0.2, 
+                            relheight = 0.05
+                            )
+            if self.labels[i][self.checkbutton]:
+                c = self.labels[i][self.checkbutton]
+                cvar = BooleanVar()
+                cvar.set(0)
+                Checkbutton(
+                            bg = 'yellow',
+                            variable = cvar, 
+                            onvalue = 1,
+                            offvalue = 0).place(
+                            relx = c['x'], 
+                            rely = c['y'],
+                            relwidth = 0.2, 
+                            relheight = 0.05,
+                            )
+                c.update({'cvar' : cvar})
+            
+
             print(self.labels[i][1])
+def push():
+    return 1
 root = Tk()
 
 root.title("settings")
 root.geometry('500x500')
-menu = Settings(l_con)
+menu = Settings(l_con,spins,checks)
 root.mainloop()
+print()
