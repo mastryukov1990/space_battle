a959817764938bed6a204746dc497b294294e495
avmmal <32042102+MakeevVlad@users.noreply.github.com>
2019-12-20 13:06:49 +0300

    Add files via upload

---
+++ image.py
@@ -0,0 +1,18 @@
+import pygame 
+
+pygame.init()
+sc = pygame.display.set_mode((400, 300)) 
+sc.fill((100, 150, 200)) 
+
+dog_surf = pygame.image.load('spaceship1.jpg') 
+dog_rect = dog_surf.get_rect(bottomright = (400, 300)) 
+sc.blit(dog_surf, dog_rect) 
+
+pygame.display.update() 
+
+while 1: 
+    for i in pygame.event.get(): 
+        if i.type == pygame.QUIT: 
+            exit() 
+
+pygame.time.delay(20)
